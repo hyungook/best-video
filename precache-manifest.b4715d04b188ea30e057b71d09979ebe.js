ddaf35994fb3877b6936a342399703854f864af9
오형욱 <nenonenoberneno@gmail.com>
2020-12-05 00:58:30 +0900

    Updates

---
+++ precache-manifest.b4715d04b188ea30e057b71d09979ebe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24fc1ee246545ccafcc44a847eb9b274",
+    "url": "/best-video/index.html"
+  },
+  {
+    "revision": "e171a2916cad2be549aa",
+    "url": "/best-video/static/css/main.e7aa0c73.chunk.css"
+  },
+  {
+    "revision": "64ee5b7c40c340c124ba",
+    "url": "/best-video/static/js/2.9b311772.chunk.js"
+  },
+  {
+    "revision": "3263eb4fa2a4ed54bb570011085a455d",
+    "url": "/best-video/static/js/2.9b311772.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e171a2916cad2be549aa",
+    "url": "/best-video/static/js/main.1685d0a5.chunk.js"
+  },
+  {
+    "revision": "8a4f8285a94ba5a20b8f",
+    "url": "/best-video/static/js/runtime-main.cdadb5f8.js"
+  }
+]);
